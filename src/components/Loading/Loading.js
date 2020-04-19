2b08992f5454452b0c6ff3bd4de696da98d8e63d
Ray Feng <ryfprivate@outlook.com>
2020-04-19 23:59:13 +1000

    Separated components into folder

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: undefined,
+    };
+  }
+}
