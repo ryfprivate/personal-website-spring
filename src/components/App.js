77c1fff65a6c9f9e445f019f7bbb934618da0d9e
Ray Feng <ryfprivate@outlook.com>
2020-04-20 00:23:05 +1000

    Installed react spring

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
-import Loading from "./Loading";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Loading loaded={loaded}></Loading>
+        <h1>Hello test</h1>
       </header>
     </div>
   );
